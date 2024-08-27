b7924fc870f52d2f3a5b1b0a725a84fc25d1d76f
Andre Campos <andrecamppos@gmail.com>
2024-08-27 23:03:40 +0100

    viewport

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Chalkboard from './Chalkboard';
 function App() {
   return (
     <div className="App">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <Chalkboard />
     </div>
   );
